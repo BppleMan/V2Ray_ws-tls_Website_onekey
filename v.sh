ba2e44d7d3f51e4685902e443d3a08a630974119
BppleMan <1286363484@qq.com>
2019-05-06 02:26:19 +0800

    Update增加证书自动更新功能

--- v.sh
+++ v.sh
@@ -651,6 +651,17 @@ start_process_systemd(){
 	judge "V2ray 启动"
 }
 
+acme_cron_update(){
+    if [[ "${ID}" == "centos" ]];then
+        sed -i "/acme.sh/c 0 0 * * 0 systemctl stop nginx && \"/root/.acme.sh\"/acme.sh --cron --home \"/root/.acme.sh\" \
+        > /dev/null && systemctl start nginx " /var/spool/cron/root
+    else
+        sed -i "/acme.sh/c 0 0 * * 0 systemctl stop nginx && \"/root/.acme.sh\"/acme.sh --cron --home \"/root/.acme.sh\" \
+        > /dev/null && systemctl start nginx " /var/spool/cron/crontabs/root
+    fi
+    judge "cron 计划任务更新"
+}
+
 #展示客户端配置信息
 show_information(){
 	clear
@@ -733,6 +744,7 @@ main_ssloff(){
 	win64_v2ray
 	show_information
 	start_process_systemd
+	acme_cron_update
 }
 
 main(){
